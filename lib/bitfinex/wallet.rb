f10e86539a44a7683887fbfabe7988e49e32bb85
Enrico Rubboli <rubboli@gmail.com>
2016-02-19 16:13:49 +0100

    add last api methods

--- lib/bitfinex/wallet.rb
+++ lib/bitfinex/wallet.rb
@@ -4,5 +4,28 @@ module Bitfinex
       check_params(params, %i{type currency amount available})
       authenticated_post("balances", params).body
     end
+
+    def margin_info
+      authenticated_post("margin_info").body
+    end
+
+    def transfer(amount, currency, wallet_from, wallet_to)
+      params = {
+        amount: amount,
+        currency: currency,
+        wallet_from: wallet_from,
+        wallet_to: wallet_to
+      }
+      authenticated_post("transfer", params).body
+    end
+
+    def withdraw(withdraw_type, walletselected, amount, params={})
+      params.merge!({
+        withdraw_type: withdraw_type,
+        walletselected: walletselected,
+        amount: amount})
+
+      authenticated_post("withdraw", params).body
+    end
   end
 end
--- spec/bitfinex/wallet_spec.rb
+++ spec/bitfinex/wallet_spec.rb
@@ -5,10 +5,39 @@ describe Bitfinex::Client do
 
   let(:response) { [ { "type": "depsit", "amount": "10.0"} ] }
 
-  before do 
-	  stub_http("/balances", response.to_json, method: :post)
-	  @response = client.balances
-	end
+  context ".balances" do
+    before do 
+      stub_http("/balances", response.to_json, method: :post)
+      @response = client.balances
+    end
 
-  it {expect(@response.size).to eq(1)}
+    it {expect(@response.size).to eq(1)}
+  end
+
+  context "margin_info" do
+    before do 
+      stub_http("/margin_info", response.to_json, method: :post)
+      @response = client.margin_info
+    end
+
+    it {expect(@response.size).to eq(1)}
+  end
+
+  context "transfer" do
+    before do 
+      stub_http("/transfer", response.to_json, method: :post)
+      @response = client.transfer(100, "USD", 1000,1001)
+    end
+
+    it {expect(@response.size).to eq(1)}
+  end
+
+  context "withdraw" do
+    before do 
+      stub_http("/withdraw", response.to_json, method: :post)
+      @response = client.withdraw("bitcoin", "deposit", 10, address: "bitcoin address")
+    end
+
+    it {expect(@response.size).to eq(1)}
+  end
 end

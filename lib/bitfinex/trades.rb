36463c5cd0b360993809840e406de50c693933a3
Enrico Rubboli <rubboli@gmail.com>
2016-02-19 16:55:00 +0100

    fix tabs

--- lib/bitfinex/trades.rb
+++ lib/bitfinex/trades.rb
@@ -1,7 +1,7 @@
 module Bitfinex
   module TradesClient
     def trades symbol="btcusd", params={}
-			check_params(params, %i{timestamp limit_trades})
+      check_params(params, %i{timestamp limit_trades})
       get("trades/#{symbol}", params: params).body
     end
   end

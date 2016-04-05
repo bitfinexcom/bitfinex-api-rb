9af4ae5ad7188d2dfe7654f55a2303b91b9c5fba
Enrico Rubboli <rubboli@gmail.com>
2016-04-05 11:40:45 +0200

    add more errors handling and bump version to release 0.0.6

--- lib/bitfinex/errors.rb
+++ lib/bitfinex/errors.rb
@@ -6,12 +6,25 @@ module Bitfinex
   class InvalidAuthKeyError < ClientError; end
   class BlockMissingError < ParamsError; end
   class ServerError < Exception; end # Error reported back by Binfinex server
+  class BadRequestError < ServerError; end
+  class NotFoundError < ServerError; end
+  class ForbiddenError < ServerError; end
+  class UnauthorizedError < ServerError; end
+  class InternalServerError < ServerError; end
 
   class CustomErrors < Faraday::Response::Middleware
     def on_complete(env)
       case env[:status]
-      when 400..500
-        raise ServerError, env.body['message']
+      when 400
+        raise BadRequestError, env.body['message']
+      when 401
+        raise UnauthorizedError
+      when 403
+        raise ForbiddenError
+      when 404
+        raise NotFoundError
+      when 500
+        raise InternalServerError
       else
         super
       end

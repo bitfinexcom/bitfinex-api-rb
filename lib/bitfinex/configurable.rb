de4ef8ce6b6ca53a03fe2f4a9619364f6f1bc12f
Enrico Rubboli <rubboli@gmail.com>
2017-01-22 15:32:02 +0100

    fix configurable and ticker example

--- lib/bitfinex/configurable.rb
+++ lib/bitfinex/configurable.rb
@@ -20,11 +20,16 @@ module Bitfinex
   end
 
   class Configuration
-    attr_accessor :api_endpoint, :debug, :debug_connection, :secret, :api_key, :websocket_api_endpoint, :rest_timeout, :rest_open_timeout
+    attr_accessor :api_endpoint, :debug, :debug_connection, :secret
+    attr_accessor :api_key, :websocket_api_endpoint, :rest_timeout
+    attr_accessor :reconnect, :reconnect_after, :rest_open_timeout
+
     def initialize
-       self.api_endpoint = "https://api.bitfinex.com/v1/"
-       self.websocket_api_endpoint = "wss://api.bitfinex.com/ws"
+      self.api_endpoint = "https://api.bitfinex.com/v1/"
+      self.websocket_api_endpoint = "wss://api.bitfinex.com/ws"
       self.debug = false
+      self.reconnect = true
+      self.reconnect_after = 60
       self.rest_timeout = 30
       self.rest_open_timeout = 30
       self.debug_connection = false

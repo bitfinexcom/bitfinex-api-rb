c30460b60c21f1a4dd7697c089b054a07045dda7
kingwkb <kingwkb@gmail.com>
2021-05-11 15:36:14 +0800

    fix books params error fix margin and funding info

--- lib/rest/v2/margin.rb
+++ lib/rest/v2/margin.rb
@@ -5,8 +5,8 @@ module Bitfinex
     #
     # @example:
     #    client.offers
-    def offers
-      authenticated_post("auth/r/offers").body
+    def offers(symbol)
+      authenticated_post("auth/r/funding/offers/#{symbol}").body
     end
 
     # Get account margin info
@@ -17,7 +17,7 @@ module Bitfinex
     # @example:
     #   client.margin_info("tBTCUSD")
     def margin_info(symbol = "base")
-      authenticated_post("auth/r/margin/#{symbol}").body
+      authenticated_post("auth/r/info/margin/#{symbol}").body
     end
 
     # Get account funding info
@@ -27,7 +27,7 @@ module Bitfinex
     # @example:
     #   client.funding_info
     def funding_info(symbol = "fUSD")
-      authenticated_post("auth/r/funding/#{symbol}").body
+      authenticated_post("auth/r/info/funding/#{symbol}").body
     end
   end
 end
